00e3fe3064dd11a4540bf2a37c15df280c81bb8b
valentinachavez <valencha10@gmail.com>
2019-09-11 22:37:27 -0500

    Informacion completa con modal

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,22 +1,175 @@
 import React from 'react';
 import {Card, CardContent, makeStyles} from '@material-ui/core';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import { useSpring, animated } from 'react-spring';
 import { flexbox } from '@material-ui/system';
+import { parseTwoDigitYear } from 'moment';
+import { bold, black } from 'ansi-colors';
 
 
 
 function WeatherCard (props){
-    const classes = useStyles();
     
-    return (<Card>
-        <CardContent className= {classes.content}>
-        <h3 className= {classes.title} >{props.day}</h3>
-        <img className= {classes.img} src={props.icon}/>
-        <p className= {classes.info}> <span>{props.max}</span>{props.min}</p>
+    const classes = useStyles();
+    const current = currentCard();
+    var colorTheme = classes;
+    
+    var myDate = new Date();
+    var date = myDate.getDay();
+    
+    
+    switch (date) {
+        
+        case 1:
+        Date= 'lun.'
+        break;
+        
+        case 2:
+        date ='mar.'
+        break;
+        case 3:
+        date ='mié.'
+        break;
+        case 4:
+        date ='jue.'
+        break;
+        case 5:
+        date ='vie.'
+        break;
+        case 6:
+        date ='sáb.'
+        break;
+        
+        
+        
+        
+    }
+    
+    
+    const [open, setOpen] = React.useState(false);
+    
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    if( date=== props.day){
+        colorTheme = current;
+    }
+    
+    return (<Card onClick={handleOpen} className= {colorTheme.general}>
+        
+        <CardContent className= {colorTheme.content}>
+        <h3 className= {colorTheme.title} >{props.day}</h3>
+        <img className= {colorTheme.img} src={props.icon}/>
+        <p className= {colorTheme.info}> <span>{props.max}</span>{props.min}</p>
         </CardContent>
         
+        <div >
         
-        </Card>);
+        <Modal 
+        aria-labelledby="spring-modal-title"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+            timeout: 500,
+        }}
+        >
+        <div onClick={handleClose} >
+        <Fade in={open}>
+        <ClickAwayListener onClickAway={handleClose}>
+        
+        <div className={colorTheme.paper}onClick={handleClose}>
+      
+        <section className={classes.principalInfo}>
+        <div> <img className= {classes.img} src={props.icon}/></div>
+        <div>
+        <h1 className= {colorTheme.title} >{props.modalDay}</h1>   
+        <h2 className= {colorTheme.title}  >{props.modalCity}, {props.modalCountry}</h2>
+        </div>
+        
+        </section>
+        <section>
+        <p ><span className= {colorTheme.subtitle} >Temp Min:</span>{props.modalMin}</p>
+        <p><span className= {colorTheme.subtitle} >Temp Max:</span>{props.modalMax}</p>
+        <p><span className= {colorTheme.subtitle} >Velocidad del viento:</span>{props.modalSpeed}</p> 
+        <p><span className= {colorTheme.subtitle} >Nivel de mar:</span> {props.modalSeaLevel}</p>
+        <p><span className= {colorTheme.subtitle} >Presión atmósferica:</span> {props.modalPressure}</p>
+        <p><span className= {colorTheme.subtitle} >Dirección del viento: </span>{props.modalDeg}</p>
+        <p><span className= {colorTheme.subtitle} >Humedad:</span> {props.modalHumidity}</p>
+        <p><span className= {colorTheme.subtitle} >Presión atmosférica sobre el nivel del suelo:</span> {props.modalGrndLevel}</p>
+        
+        <p> <span className= {colorTheme.subtitle} >Descripción del clima:</span> { props.modalDescription}</p>
+        </section>
+        </div>
+        </ClickAwayListener>
+        </Fade>
+        </div>
+        </Modal>
+        
+        </div>
+        
+        </Card>
+        
+        );
+        
+        
     }
+    const currentCard = makeStyles(theme =>({
+        principalInfo:{
+            
+            display:'flex',
+            flexDirection: 'column',
+            marginRight: 50,
+            
+        },
+        subtitle:{
+            fontWeight: 'bold',
+            
+        },
+  
+        general:{
+            
+            backgroundColor: 'orange',
+            
+        },
+        title:{
+            color:  'white',
+            fontSize: 30,
+            margin: 0,
+            
+        },
+      
+        content:{
+            display: 'flex',
+            color:  'white',
+            flexDirection: 'column',
+            alignItems: 'center',
+            
+        },
+        paper: {
+            display:'flex',
+            width: 600,
+            flexDirection: 'row',
+            backgroundColor: 'orange',
+            border: 'none',
+            borderRadius: 10,
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+            color: 'white',
+            outline:'none',
+        },
+        
+        
+    }));
     const useStyles = makeStyles(theme => ({
         title:{
             color:  theme.palette.grey[700],
@@ -31,8 +184,36 @@ function WeatherCard (props){
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-
+            
         },
+        modal: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row',
+        },
+        paper: {
+            display:'flex',
+            width: 600,
+            flexDirection: 'row',
+            backgroundColor: theme.palette.background.paper,
+            border: 'none',
+            borderRadius: 10,
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+        principalInfo:{
+            
+            display:'flex',
+            flexDirection: 'column',
+            marginRight: 50,
+            
+        },
+        subtitle:{
+            fontWeight: 'bold',
+            
+        },
+        
         info:{
             margin: 0,
             fontSize: 20,
@@ -43,4 +224,27 @@ function WeatherCard (props){
         }
         
     }));
-    export default WeatherCard;
+    const Fade = React.forwardRef(function Fade(props, ref) {
+        const { in: open, children, onEnter, onExited, ...other } = props;
+        const style = useSpring({
+            from: { opacity: 0 },
+            to: { opacity: open ? 1 : 0 },
+            onStart: () => {
+                if (open && onEnter) {
+                    onEnter();
+                }
+            },
+            onRest: () => {
+                if (!open && onExited) {
+                    onExited();
+                }
+            },
+        });
+        
+        return (
+            <animated.div ref={ref} style={style} {...other}>
+            {children}
+            </animated.div>
+            );
+        });
+        export default WeatherCard;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,89 +1,107 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import WeatherCard from '../../components/WeatherCard/WeatherCard';
-import {Grid,CssBaseline } from '@material-ui/core';
+import {Grid,CssBaseline,Button, Modal} from '@material-ui/core';
+import moment from 'moment';
 
-const days = [
-  
-  {
-    day : 'Wed',
-    icon : "http://openweathermap.org/img/wn/02d@2x.png",
-    max: "70º",
-    min:"60º",
-  },
-  
-  {
-    day: "Thu",
-    icon : "http://openweathermap.org/img/wn/03d@2x.png",
-    max: "60º",
-    min:"70º",
-  },
-  {
-    day: "Fri",
-    icon :"http://openweathermap.org/img/wn/10d@2x.png",
-    max: "50º",
-    min:"40º",
-  },
-  
-  {
-    day: "Sa",
-    icon : "http://openweathermap.org/img/wn/03d@2x.png",
-    max: "50º",
-    min:"40º",
-  },
-  
-  {
-    day: "Sun",
-    icon : "http://openweathermap.org/img/wn/01d@2x.png",
-    max: "50º",
-    min:"40º",
-  },
-  
-  {
-    day: "Mon",
-    icon : "http://openweathermap.org/img/wn/02d@2x.png",
-    max: "60º",
-    min:"50º",
-  },
-  
-  {
-    day: "Tus",
-    icon: "http://openweathermap.org/img/wn/03d@2x.png",
-    max:"70º",
-    min:"60º",
-  },
-  {
-    day: "Wed",
-    icon: "http://openweathermap.org/img/wn/01d@2x.png",
-    max: "50º",
-    min: "40º",
-  }
-  
-  
-];
+import 'moment/locale/es';
+import { element } from 'prop-types';
+
+moment.locale('es');
 
 
 function App() {
-  return (<div>  
-    <Grid container spacing = {3}>
+  const [days, setDays] = React.useState([]);
+  
+  const handleGetWeather= ()=>{
+    console.log('get weather');
+    var promise = fetch(' http://api.openweathermap.org/data/2.5/forecast?q=Cali,co&APPID=905301d7de1425eb8fd9571e4dff0b11&lang=es');
+    promise.then((info)=>{
+      return info.json();
+    }).then((info)=>{
+      
+      var list = info.list.filter((elem, index)=>{
+        return index %8 ===0;
+        
+      }); 
+      
+      
+      
+      list = list.map((elem, index,array) =>{
+        var city =info.city.name;
+        var country =info.city.country;
+        
+
+        return {
+          day : moment.unix(elem.dt).format('ddd'),
+          modalDay : moment.unix(elem.dt).format('dddd'),
+          modalCountry : country,
+          modalCity : city,
+          modalMin:  Math.round(elem.main.temp_min - 273.15 )+'º',
+          modalMax:    Math.round(elem.main.temp_max - 273.15 )+'º',
+          modalSpeed: elem.wind.speed,
+          modalSeaLevel : elem.main.sea_level,
+          modalPressure : elem.main.pressure,
+          modalDeg : elem.wind.deg,
+          modalHumidity : elem.main.humidity,
+          modalGrndLevel : elem.main.grnd_level,
+          modalDescription :elem.weather[0].description,
+          icon: `http://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png`,
+          min:  Math.round(elem.main.temp_min - 273.15 )+'º',
+          max:  Math.round(elem.main.temp_max - 273.15 )+'º',
+        };
+        
+        
+      }); 
+      
+      setDays(list);
+      
+      console.log(days);
+    });
     
-    {days.map((item) => {
-      return  <Grid item md={2}>
+  }
+  
+  
+  return (<div>  
+    <Button onClick={handleGetWeather} variant= "contained" color= "primary">
+    Get weather info
+    </Button>
+    
+    <Grid container spacing = {3}>
+     
+    
+    {days && days.map((item) => {
+   
+      return  <Grid item md={2} key={item.day}  >
       <WeatherCard 
       day={item.day}
+      dayI={item.dayI}
       icon= {item.icon}
       min = {item.min}
       max= {item.max}
+      modalDay={item.modalDay}
+      modalCountry = {item.modalCountry}
+      modalCity= {item.modalCity}
+      modalMin = {item.modalMin}
+      modalMax = {item.modalMax}
+      modalSpeed = {item.modalSpeed}
+      modalSeaLevel = {item.modalSeaLevel}
+      modalPressure = {item.modalPressure}
+      modalDeg = {item.modalDeg}
+      modalHumidity = {item.modalHumidity}
+      modalGrndLevel ={item.modalGrndLevel}
+      modalDescription = {item.modalDescription}
       />
+      
+      
       </Grid>
     }) }
-    
-    
-    
     
     </Grid>
     <CssBaseline/>
     </div>  );
   }
+  
   
   export default App;
   
